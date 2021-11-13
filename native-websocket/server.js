d8c8d6e4b91c8e683871aa0260d54cd5f258697c
ChoiKiseok <42999545+ChoiKiseok@users.noreply.github.com>
2021-11-13 17:37:30 +0900

    웹소켓 테스트중

--- native-websocket/server.js
+++ native-websocket/server.js
@@ -1,14 +1,11 @@
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port : 3008}); //서버 생성
-var id_num = 0;
 
 wss.on('connection', ws=>{      //요청 이벤트 처리
     console.log("new connect!");
     ws.send("환영합니다.");
-    wss.clients.forEach(e => {
-        console.log(e.id);
-    })
+    
     ws.on('message', data=>{    //메시지 이벤트 처리
         var text = data.toString();
         var context;
@@ -17,9 +14,22 @@ wss.on('connection', ws=>{      //요청 이벤트 처리
         } catch (e) {
             console.log(e);
         }
+        // 아이디 정보 입력
         if(context.type == "createId") {
             ws.id = context.content;
             console.log("회원가입 완료");
+            var req = {};
+            req.id = ws.id;
+            req.type = "success";
+
+            req = JSON.stringify(req);
+            ws.send(req);
+        } else {
+            //메세지 들어오면 모든 소켓에 전송
+            wss.clients.forEach(e => {
+                console.log(e.id);
+                e.send("id: " + e.id + " msg: " + text);
+            });
         }
     });
     ws.on('error', (err) => { // 에러 발생 시
